d06c111e5b8efe188b440152929911d5cc24501f
Sasha <alex_b_lime@icloud.com>
2020-08-03 22:42:52 +0300

    *

--- src/components/reviews/reviews.jsx
+++ src/components/reviews/reviews.jsx
@@ -10,34 +10,39 @@ const Reviews = (props) => {
   const [firstCol, secondCol] = splitList;
 
   return <div className="movie-card__reviews movie-card__row">
-    <div className="movie-card__reviews-col">
-      {
-        firstCol.map(({comment, date, id, rating, user}) => <div key={id} className="review">
-          <blockquote className="review__quote">
-            <p className="review__text">{comment}</p>
-            <footer className="review__details">
-              <cite className="review__author">{user.name}</cite>
-              <time className="review__date" dateTime={new Date(date).toLocaleDateString()}>{new Date(date).toLocaleDateString(`en-EN`, dateOptions)}</time>
-            </footer>
-          </blockquote>
-          <div className="review__rating">{rating}</div>
-        </div>)
-      }
-    </div>
-    <div className="movie-card__reviews-col">
-      {
-        secondCol.map(({comment, date, id, rating, user}) => <div key={id} className="review">
-          <blockquote className="review__quote">
-            <p className="review__text">{comment}</p>
-            <footer className="review__details">
-              <cite className="review__author">{user.name}</cite>
-              <time className="review__date" dateTime={new Date(date).toLocaleDateString()}>{new Date(date).toLocaleDateString(`en-EN`, dateOptions)}</time>
-            </footer>
-          </blockquote>
-          <div className="review__rating">{rating}</div>
-        </div>)
-      }
-    </div>
+    {
+      commentList.length !== 0 && <>
+        <div className="movie-card__reviews-col">
+          {
+            firstCol.map(({comment, date, id, rating, user}) => <div key={id} className="review">
+              <blockquote className="review__quote">
+                <p className="review__text">{comment}</p>
+                <footer className="review__details">
+                  <cite className="review__author">{user.name}</cite>
+                  <time className="review__date" dateTime={new Date(date).toLocaleDateString()}>{new Date(date).toLocaleDateString(`en-EN`, dateOptions)}</time>
+                </footer>
+              </blockquote>
+              <div className="review__rating">{rating}</div>
+            </div>)
+          }
+        </div>
+        <div className="movie-card__reviews-col">
+          {
+            secondCol.map(({comment, date, id, rating, user}) => <div key={id} className="review">
+              <blockquote className="review__quote">
+                <p className="review__text">{comment}</p>
+                <footer className="review__details">
+                  <cite className="review__author">{user.name}</cite>
+                  <time className="review__date" dateTime={new Date(date).toLocaleDateString()}>{new Date(date).toLocaleDateString(`en-EN`, dateOptions)}</time>
+                </footer>
+              </blockquote>
+              <div className="review__rating">{rating}</div>
+            </div>)
+          }
+        </div>
+      </>
+      || <p style={{color: `#0c0101`}}>No reviews yet...</p>
+    }
   </div>;
 };
 
