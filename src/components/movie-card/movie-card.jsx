5367db5de625e2840d5d51162699467c39ac78ac
Sasha <alex_b_lime@icloud.com>
2020-08-03 18:32:34 +0300

    fix movie card

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,18 +1,26 @@
 import {Link} from 'react-router-dom';
 import MoviePlayer from 'components/movie-player/movie-player.jsx';
+import {useHistory} from 'react-router-dom';
 
 const MovieCard = (props) => {
   const {movie, onClick} = props;
+  const {push} = useHistory();
+
+  const goToMovie = (path) => {
+    push(path);
+  };
 
   return (
     <article
       className="small-movie-card catalog__movies-card"
       onClick={() => onClick(movie)}
     >
-      <MoviePlayer movie={movie} />
-      <h3 className="small-movie-card__title">
-        <Link className="small-movie-card__link" to={`/films/${movie.id}`}>{movie.name}</Link>
-      </h3>
+      <div onClick={() => goToMovie(`/films/${movie.id}`)}>
+        <MoviePlayer movie={movie} />
+        <h3 className="small-movie-card__title">
+          <Link className="small-movie-card__link" to={`/films/${movie.id}`}>{movie.name}</Link>
+        </h3>
+      </div>
     </article>
   );
 };
