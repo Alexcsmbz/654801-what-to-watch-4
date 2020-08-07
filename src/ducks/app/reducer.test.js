61d4ebc8e24f7c6534f361705d3f9e0e43ea5be3
Sasha <alex_b_lime@icloud.com>
2020-08-07 12:17:39 +0300

    fix fucking travis stupid lint

--- src/ducks/app/reducer.test.js
+++ src/ducks/app/reducer.test.js
@@ -1,7 +1,6 @@
 import ActionType from './action-types.js';
 import reducer from './reducer.js';
 import {createAPI} from 'middleware/thunks.js';
-import {adapterKeys} from 'utils/utils.js';
 import MockAdapter from 'axios-mock-adapter';
 import configureStore from 'redux-mock-store';
 import {testMovie, testMovies, testComments} from 'config';
