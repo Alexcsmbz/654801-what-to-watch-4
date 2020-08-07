ed5b31e11248e4ced55999cba319987599466d61
Sasha <alex_b_lime@icloud.com>
2020-08-07 12:04:01 +0300

    add app reducer tests

--- src/ducks/app/reducer.test.js
+++ src/ducks/app/reducer.test.js
@@ -4,6 +4,7 @@ import {createAPI} from 'middleware/thunks.js';
 import {adapterKeys} from 'utils/utils.js';
 import MockAdapter from 'axios-mock-adapter';
 import configureStore from 'redux-mock-store';
+import {testMovie, testMovies, testComments} from 'config';
 
 const api = createAPI(() => {});
 const mockStore = configureStore([]);
@@ -42,5 +43,75 @@ describe(`Reducer test`, () => {
     expect(reducer({isLoading: true},
         {type: ActionType.STOP_LOADING}
     )).toEqual({isLoading: false});
+  });
+
+  it(`Reducer should load promo movie`, () => {
+    expect(reducer({promoMovie: {}},
+        {
+          type: ActionType.GET_PROMO_MOVIE,
+          payload: testMovie,
+        }
+    )).toEqual({promoMovie: testMovie});
+  });
+
+  it(`Reducer should load movies`, () => {
+    expect(reducer({movies: []},
+        {
+          type: ActionType.GET_MOVIES_SUCCESS,
+          payload: testMovies,
+        }
+    )).toEqual({
+      movies: testMovies,
+      filteredMovies: testMovies,
+      genres: [`All genres`, ...new Set(testMovies.map((movie) => movie.genre))],
+    });
+  });
+
+  it(`Reducer should get movie list by genre`, () => {
+    expect(reducer({movies: testMovies},
+        {
+          type: ActionType.GET_MOVIE_LIST_BY_GENRE,
+          payload: `genre`,
+        }
+    )).toEqual({
+      filteredMovies: testMovies,
+      movies: testMovies,
+    });
+  });
+
+  it(`Reducer should set error if load failed`, () => {
+    expect(reducer({errors: []},
+        {
+          type: ActionType.GET_MOVIES_FAILED,
+          payload: `network error`,
+        }
+    )).toEqual({errors: [`network error`]});
+  });
+
+  it(`Reducer should set favorite movie`, () => {
+    expect(reducer({addedMovies: testMovies},
+        {
+          type: ActionType.TOGGLE_MOVIE_IN_LIST,
+          payload: testMovie,
+        }
+    )).toEqual({addedMovies: []});
+  });
+
+  it(`Reducer should load favorite movies`, () => {
+    expect(reducer({addedMovies: testMovies},
+        {
+          type: ActionType.GET_FAVORITE_MOVIES,
+          payload: testMovie,
+        }
+    )).toEqual({addedMovies: testMovie});
+  });
+
+  it(`Reducer should load comment list`, () => {
+    expect(reducer({commentList: []},
+        {
+          type: ActionType.GET_COMMENT_LIST,
+          payload: testComments,
+        }
+    )).toEqual({commentList: testComments});
   });
 });
